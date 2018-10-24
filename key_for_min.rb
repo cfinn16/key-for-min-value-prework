62576e290c4c66138c0625d9a09e2a4d6c1fd303
Connor Finnegan <connorfinnegan16@gmail.com>
2018-10-24 22:57:42 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,9 +6,12 @@ def key_for_min_value(name_hash)
   name_hash.each do |name, number|
     smallest = 100000
     if number < smallest
-      number = smallest
-      smallest
+      number = new_smallest
+      if number < smallest && number < new_smallest
+        number = new_smallest
+      end
     end
+  end
   
   if number == smallest
     name
